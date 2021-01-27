491995cb51a7262593acbf1b142c9ddfeb4491cb
ShyroVO <Bosquet.laurie@outlook.com>
2021-01-27 13:21:40 +0100

    1.0

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
 // Premiere ligne
+echo date("l");
+echo "<br>";
 
+$date1 = "2018.12.10";
+echo $date1;
+echo date("m.d.y", "12.10.2018");
+echo "<br>";
 
+$date2 = "11:35:07";
+echo $date2;
+echo date("H:i:s", "11:35:07");
+echo "<br>";
 
 
 
